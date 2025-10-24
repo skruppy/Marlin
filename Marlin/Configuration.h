21a31d9fae6ae57b69bfcd2ddff046f46a3aa6b9
Skruppy <skruppy@onmars.eu>
2025-10-24 03:34:04 +0200

    [cfg] increase probe clearance/backoff from 5mm to 20mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1321,8 +1321,8 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   50 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
-#define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
+#define Z_CLEARANCE_BETWEEN_PROBES  20 // Z Clearance between probe points
+#define Z_CLEARANCE_MULTI_PROBE     20 // Z Clearance between multiple probes
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -5 // Farthest distance below the trigger-point to go before stopping
