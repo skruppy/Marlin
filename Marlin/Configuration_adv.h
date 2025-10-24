012a1d10bf0f9a7e6a160ed82f843b1c32d0348b
Skruppy <skruppy@onmars.eu>
2025-10-24 02:08:30 +0200

    [cfg] make finer fine-moves (cut distance in half to 0.0125mm)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1493,7 +1493,7 @@
 
 #if HAS_MANUAL_MOVE_MENU
   #define MANUAL_FEEDRATE { 25*60, 25*60, 25*60, 1*60 } // (mm/min) Feedrates for manual moves along X, Y, Z, E from panel
-  #define FINE_MANUAL_MOVE 0.025    // (mm) Smallest manual move (< 0.1mm) applying to Z on most machines
+  #define FINE_MANUAL_MOVE 0.0125    // (mm) Smallest manual move (< 0.1mm) applying to Z on most machines
   #if IS_ULTIPANEL
     #define MANUAL_E_MOVES_RELATIVE // Display extruder move distance rather than "position"
     #define ULTIPANEL_FEEDMULTIPLY  // Encoder sets the feedrate multiplier on the Status Screen
