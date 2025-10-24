a8e56dadf7f0238af68f487edfa9db0fc1ae7acb
Skruppy <skruppy@onmars.eu>
2025-10-24 03:36:15 +0200

    [cfg] switch from 5 to 13 bed leveling points in a row

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2256,7 +2256,7 @@
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
   #define MESH_INSET 10              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 5      // Don't use more than 15 points per axis, implementation limited.
+  #define GRID_MAX_POINTS_X 13      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   //#define UBL_HILBERT_CURVE       // Use Hilbert distribution for less travel when probing multiple points
