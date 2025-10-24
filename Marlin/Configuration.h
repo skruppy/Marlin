9d09acc3e19961cce02b00d87fbe53179dc0d8af
Skruppy <skruppy@onmars.eu>
2025-10-24 03:31:13 +0200

    [cfg] change z probe offset from -14.60 to a more save default of -16.13

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1252,7 +1252,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -14.60 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -16.13 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
