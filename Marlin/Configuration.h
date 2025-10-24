212f59251bcd853ece17efd376a2a8dcb6e0310e
Skruppy <skruppy@onmars.eu>
2025-10-24 03:31:13 +0200

    [cfg] change z probe offset from -14.60 to a more save default of -16.13

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1724,7 +1724,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -14.60 } // (mm) X, Y, Z distance from Nozzle tip to Probe trigger-point
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -16.13 } // (mm) X, Y, Z distance from Nozzle tip to Probe trigger-point
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
